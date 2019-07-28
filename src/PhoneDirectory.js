cb248fd783f10f4e9be5d22503c3c77534aaf787
Siddhant Sambit <siddhantsambit@gmail.com>
2019-07-28 18:22:39 +0530

    Added routing for Add Subscriber page and Show Subscribers page

--- src/PhoneDirectory.js
+++ src/PhoneDirectory.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import AddSubscriber from './AddSubscriber';
 import ShowSubscribers from './ShowSubscribers';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class PhoneDirectory extends Component {
 
@@ -11,7 +12,7 @@ class PhoneDirectory extends Component {
                 id: 1,
                 name: 'Shilpa Bhat',
                 phone: '8888888888'
-            }, 
+            },
             {
                 id: 2,
                 name: 'Shristi Gupta',
@@ -30,14 +31,20 @@ class PhoneDirectory extends Component {
         }
         subscribersList.push(newSubscriber);
         this.setState({ subscribersList: subscribersList });
-        console.log("Phone Directory");
-        console.log(this.state.subscribersList);
+        //console.log("Phone Directory");
+        //console.log(this.state.subscribersList);
     }
 
     render() {
         return (
+            <Router>
+                <div>
+                    <Route exact path='/' render={(props) => <ShowSubscribers {...props} subscribersList={this.state.subscribersList} />} />
+                    <Route exact path='/add' render={({history},props) => <AddSubscriber history={history} {...props} addSubscriberHandler={this.addSubscriberHandler} />} />
+                </div>
+            </Router>
             //<AddSubscriber addSubscriberHandler={this.addSubscriberHandler} />
-            <ShowSubscribers subscribersList={this.state.subscribersList}></ShowSubscribers>
+            //<ShowSubscribers subscribersList={this.state.subscribersList}></ShowSubscribers>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ShowSubscribers from './ShowSubscribers';
-import AddSubscriber from './AddSubscriber';
+//import ShowSubscribers from './ShowSubscribers';
+//import AddSubscriber from './AddSubscriber';
 import * as serviceWorker from './serviceWorker';
 import './common/common.css';
 import PhoneDirectory from './PhoneDirectory';
